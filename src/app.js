01ebda499092fa651a8bba864e41bd90a5122868
Perfect QA <mr.vinitprajapati07@gmail.com>
2025-09-03 12:46:31 +0530

    created get apis

--- src/app.js
+++ src/app.js
@@ -15,6 +15,30 @@ app.post("/signup",async (req, res) => {
   }
 });
 
+app.get("/user",async(req,res)=>{
+   const userEmail = req.body.emailId
+  try {
+    const user = await User.find({emailId : userEmail});
+    if(user.length === 0){
+      res.status(404).send("User not found");
+    }
+    else{
+      res.send(user);
+    }
+  } catch (error) {
+    res.status(400).send("Something went wrong"+error.message);
+  }
+})
+
+app.get("/feed",async(req,res)=>{
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(500).send("Error fetching feed: " + error.message);
+  }
+})
+
 connectDB()
   .then(() => {
     console.log("Database connected successfully");
